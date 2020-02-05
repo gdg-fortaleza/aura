05a7251a4529f318bf2034b4e1ab9447ee93fe94
Nathannael <nathannael2009@gmail.com>
2020-02-04 22:52:36 -0300

    Add supporters tab

--- src/components/home/partners.vue
+++ src/components/home/partners.vue
@@ -63,6 +63,62 @@
             </v-flex>
 
             <v-flex xs12 md12 class="mt-3">
+
+                <p class="google-font my-2" style="font-size:110%">Apoiadores</p>
+
+               <v-layout row wrap class="transparent">
+                    <v-flex xs12 sm7 class="elevation-0 transparent">
+                        <v-card class="elevation-0 pa-0 ma-0 transparent">
+                            <v-container grid-list-sm fluid class="pa-0 ma-0">
+                                <v-layout row wrap class="ma-0 pa-0">
+                                    <v-flex
+                                        v-for="(itemp,i) in supporterData" :key="i"
+                                        xs6
+                                        md3
+                                        sm4
+                                        d-flex
+                                        class="pa-0 white elevation-0"
+                                    >
+                                    <!-- style="border-radius:5px;border-color:#e0e0e0;border-width: 1px; border-style: solid;" -->
+                                    <v-card flat tile class="d-flex pa-2 ma-1 elevation-1" style="border-radius:4px;border-width: 0px; border-style: solid;">
+
+                                        <a v-bind:href="itemp.link" target="_blank" >
+                                            <v-tooltip bottom>
+                                            <template v-slot:activator="{ on }">
+                                            <v-img
+                                            :src="getImgUrl(itemp.img)"
+                                            :lazy-src="getImgUrl(itemp.img)"
+                                            aspect-ratio="3.0"
+                                            class="white"
+                                            v-on="on"
+                                            >
+                                                <v-layout
+                                                    slot="placeholder"
+                                                    fill-height
+                                                    align-center
+                                                    justify-center
+                                                    ma-0
+                                                >
+                                                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                                </v-layout>
+                                            </v-img>
+                                            </template>
+                                            <span class="google-font">{{itemp.name}}</span>
+                                            </v-tooltip>
+                                        </a>
+
+                                    </v-card>
+                                    </v-flex>
+
+                                </v-layout>
+                            </v-container>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+
+            </v-flex>
+
+            <v-flex xs12 md12 class="mt-3">
                 <p class="google-font my-2" style="font-size:110%">Parceiro de Template</p>
 
                 <v-layout class="transparent" >
@@ -112,69 +168,19 @@
                 </v-layout>
 
             </v-flex>
-
-            <!-- v-flex xs12 md12 class="mt-3">
-
-                <p class="google-font my-2" style="font-size:110%">Modelo de parceiro</p>
-
-                <v-layout class="transparent" >
-                    <v-flex xs12 sm11 class="elevation-0 transparent">
-                        <v-card class="elevation-0 pa-0 ma-0 transparent">
-                            <v-container grid-list-sm fluid class="pa-0 ma-0">
-                                <v-layout row wrap class="ma-0 pa-0">
-                                    <v-flex
-                                        xs6
-                                        md2
-                                        sm4
-                                        d-flex
-                                        class="white elevation-0"
-                                    >
-                                    <v-card flat tile class="d-flex pa-2 ma-1 elevation-1" style="border-radius:5px;border-width: 1px; border-style: solid;">
-                                        <a href="https://gdgfortaleza.com.br" target="_blank">
-                                        <v-tooltip bottom>
-                                            <template v-slot:activator="{ on }">
-                                        <v-img
-                                            :src="require('@/assets/img/partners/gdgfortaleza.png')"
-                                            :lazy-src="require('@/assets/img/partners/gdgfortaleza.png')"
-                                            aspect-ratio="3.0"
-                                            class="white"
-                                            v-on="on"
-                                        >
-                                            <v-layout
-                                                slot="placeholder"
-                                                fill-height
-                                                align-center
-                                                justify-center
-                                                ma-0
-                                            >
-                                                <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                                            </v-layout>
-                                        </v-img>
-                                        </template>
-                                        <span class="google-font">GDG Fortaleza</span>
-                                        </v-tooltip>
-                                        </a>
-                                    </v-card>
-                                    </v-flex>
-
-                                </v-layout>
-                            </v-container>
-                        </v-card>
-                    </v-flex>
-                </v-layout> ->
-
-            </v-flex -->
         </v-layout>
     </v-container>
 </template>
 
 <script>
 import datap from '@/assets/data/partners.json'
+import datas from '@/assets/data/supporters.json'
 import chapterDetails from '@/assets/data/chapterDetails.json'
 export default {
     data() {
         return {
             partnerData: datap,
+            supporterData: datas,
             chapterDetails: chapterDetails
         }
     },
